1d445d677f93ec025b4285c689c31ba310454024
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-18 13:36:49 +0100

    define navbar component

---
+++ src/components/navBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <h1>JobFinder</h1>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
