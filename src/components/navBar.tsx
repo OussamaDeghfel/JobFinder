dfc73b876bc651bdf692afbf66eb38774e67c6af
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-18 14:22:33 +0100

    navbar edition

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import { Switch } from 'antd'
+import { BiMoon, BiSun } from 'react-icons/bi'
 
 const NavBar = () => {
   return (
-    <div>
-        <h1>JobFinder</h1>
-        <div>
-            
+    <div className='flex w-full bg-gray-500 p-5 rounded-b-full'>
+        <div className='flex w-[980px] h-12 justify-between items-center m-auto '>
+        <h1 className='text-2xl font-bold text-white'>JobFinder</h1>
+        <div className='flex space-x-2'>
+            <span className=''> <BiSun size={20} color='white' /> </span>
+            <Switch defaultChecked size='default' />
+            <span className=''> <BiMoon size={20} color='white' /> </span>
+        </div>
         </div>
     </div>
   )
