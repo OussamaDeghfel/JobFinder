3ab56b717aa5be348323fd333eb511ca9bb589c0
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-18 15:34:15 +0100

    define filter component and import in the navbar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,9 +1,10 @@
 import { Switch } from 'antd'
 import { BiMoon, BiSun } from 'react-icons/bi'
+import Filters from './filters'
 
 const NavBar = () => {
   return (
-    <div className='flex w-full bg-gray-500 p-5 rounded-b-full'>
+    <div className='flex flex-col w-full bg-gray-500 p-5 rounded-b-full'>
         <div className='flex w-[980px] h-12 justify-between items-center m-auto '>
         <h1 className='text-2xl font-bold text-white'>JobFinder</h1>
         <div className='flex space-x-2'>
@@ -11,6 +12,9 @@ const NavBar = () => {
             <Switch defaultChecked size='default' />
             <span className=''> <BiMoon size={20} color='white' /> </span>
         </div>
+        </div>
+        <div className='  items-center m-auto w-[50%]'>
+            <Filters />
         </div>
     </div>
   )
