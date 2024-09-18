2afc6ee4e340d13543ed1d778f6bbd2c8243635b
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-18 16:52:17 +0100

    refactor the form.item

--- src/components/filters.tsx
+++ src/components/filters.tsx
@@ -1,38 +1,50 @@
-import { Button, Form, Select } from "antd";
+import { Button, Form, Input, Select } from "antd";
 import { BiSearch } from "react-icons/bi";
 import { FaLocationDot } from "react-icons/fa6";
 
 const Filters = () => {
   return (
-    <Form className="flex bg-white h-16 pt-4 rounded-md justify-evenly items-center w-full border-2 border-black">
-      
-      <Form.Item>
-        <div className="flex m-2 space-x-2">
-          <BiSearch size={20} color="blue" />
-          <input type="text" placeholder="Filter By Title..." />
-        </div>
-      </Form.Item>
+    <Form className="flex translate-y-3 px-5 bg-white h-20 pt-4 rounded-md justify-rounded items-center w-full shadow-lg">
+      <Form.Item className="p-2 border-r-2">
         
-      <Form.Item>
-        <div className="flex space-x-2">
-          <FaLocationDot size={20} color="blue" />
-          <input type="text" placeholder="Filter By Location..." />
+          <Input
+            type="text"
+            placeholder="Filter By Title..."
+            prefix={<BiSearch size={20} color="blue" />}
+          />
+        
+      </Form.Item>
+
+      <Form.Item className="p-2 border-r-2">
+        
+          <Input
+            type="text"
+            placeholder="Filter By Location..."
+            prefix={<FaLocationDot size={20} color="blue" />}
+          />
+        
+      </Form.Item>
+
+      <Form.Item className="p-2 border-r-2">
+        <div>
+          <Select defaultValue={"fullTime"} options={
+            [
+              {
+                value: "fullTime",
+                label: "Full Time"
+              },
+              {
+                value: "partTime",
+                label: "Part Time"
+              }
+            ]
+          } />
         </div>
       </Form.Item>
-      
-      <Form.Item>
-        <div className="w-ful">
-          <Select defaultValue={"fullTime"}>
-            <Select.Option value="fullTime">Full Time</Select.Option>
-            <Select.Option value="partTime">Part Time</Select.Option>
-          </Select>
-        </div>
+
+      <Form.Item className="p-2">
+        <Button type="primary">Search</Button>
       </Form.Item>
-      
-      <Form.Item>
-        <Button className="bg-blue-500 text-white m-4">Search</Button>
-      </Form.Item>
-      
     </Form>
   );
 };
