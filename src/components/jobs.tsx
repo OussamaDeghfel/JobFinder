031b47383f20ae47618f777e4071cbfbacfbe89d
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-18 17:47:43 +0100

    add jobcard component and pass the data through it

--- src/components/jobs.tsx
+++ src/components/jobs.tsx
@@ -1,44 +1,47 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
+import JobCard from "./jobCard";
 
-interface dataJobType {
-  title: string
-  location: string
-  work_method: string
-  description: string
-  salary_range: string
+export interface dataJobType {
+  id: number;
+  title: string;
+  location: string;
+  work_method: string;
+  description: string;
+  time_posted: string;
 }
 
 const Jobs = () => {
-  const [jobs, setJobs] = useState<dataJobType[]>([])
-  
+  const [jobs, setJobs] = useState<dataJobType[]>([]);
 
   useEffect(() => {
-    fetch('././jobData.json')
-      .then(response => {
+    fetch("././jobData.json")
+      .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         return response.json();
       })
-      .then(data => setJobs(data))
-      .catch(error => console.log('Fetch error: ', error));
+      .then((data) => setJobs(data))
+      .catch((error) => console.log("Fetch error: ", error));
   }, []);
 
-  console.log(jobs)
+  console.log(jobs);
 
- return (
-    <div className='border-2 border-red-500'>
+  return (
+    <div className="grid grid-cols-3 justify-center place-items-center translate-y-20">
       {jobs?.map((job) => (
-        <div key={job.title}>
-          <p>{job.title}</p>
-          <p>{job.location}</p>
-          <p>{job.work_method}</p>
-          <p>{job.description}</p>
-          <p>{job.salary_range}</p>
+        <div key={job.id}>
+          <JobCard
+            title={job.title}
+            location={job.location}
+            work_method={job.work_method}
+            description={job.description}
+            time_posted={job.time_posted}
+          />
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Jobs
+export default Jobs;
