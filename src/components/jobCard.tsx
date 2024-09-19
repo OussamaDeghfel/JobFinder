4ff12d06221d30582e2dbf15ae401dd801f2507e
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-19 10:39:55 +0100

    add apply button

--- src/components/jobCard.tsx
+++ src/components/jobCard.tsx
@@ -1,3 +1,12 @@
+import { Button } from "antd";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+
+// declare module 'dayjs' {
+//     interface Dayjs {
+//         fromNow(withoutSuffix?: boolean): string
+//     }
+// }
 
 interface jobData {
   title: string;
@@ -14,18 +23,34 @@ const JobCard = ({
   description,
   time_posted,
 }: jobData) => {
+  const [dateAgo, setDateAgo] = useState("");
+
+  const ChangeDateFormat = (time_posted: string) => {
+    const date = new Date();
+    const changeDate = dayjs(time_posted).format("YYYY-MM-DD HH:mm:ss");
+    console.log(changeDate);
+    // const dateAgoCalucated = dayjs(changeDate).fromNow()
+    console.log(date);
+    // setDateAgo(dateAgoCalucated)
+  };
+
+  useEffect(() => {
+    ChangeDateFormat(time_posted);
+  }, [time_posted]);
+
   return (
     <div className="border-2 border-blue-100 w-96 space-y-4 rounded-md p-4 m-4 shadow-md">
       <div className="flex items-center text-gray-500">
-        <span>{time_posted}</span>
+        <span>{dateAgo}</span>
         <span className="px-4 font-bold">.</span>
         <span>{work_method}</span>
       </div>
       <h1 className="text-3xl font-bold">{title}</h1>
-      <div>
-        <p className="text-gray-500">{description}</p>
+      <p className="text-gray-500">{description}</p>
+      <div className="flex justify-between">
+        <h1 className="text-blue-600 text-sm font-bold">{location}</h1>
+        <Button type="primary" size="large">Apply Now</Button>
       </div>
-      <h1 className="text-blue-600 font-bold">{location}</h1>
     </div>
   );
 };
