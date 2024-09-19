578ac1705eb7e7ea6a37021ba6f794a3d787ac0e
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-19 11:25:59 +0100

    add logic of the difference between postDate and the currentDate

--- src/components/jobCard.tsx
+++ src/components/jobCard.tsx
@@ -1,12 +1,10 @@
 import { Button } from "antd";
 import dayjs from "dayjs";
 import { useEffect, useState } from "react";
-
-// declare module 'dayjs' {
-//     interface Dayjs {
-//         fromNow(withoutSuffix?: boolean): string
-//     }
-// }
+import relativeTime from "dayjs/plugin/relativeTime";
+import duration from "dayjs/plugin/duration";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
 
 interface jobData {
   title: string;
@@ -24,14 +22,40 @@ const JobCard = ({
   time_posted,
 }: jobData) => {
   const [dateAgo, setDateAgo] = useState("");
+  dayjs.extend(relativeTime);
+  dayjs.extend(duration);
+  dayjs.extend(utc);
+dayjs.extend(timezone);
 
   const ChangeDateFormat = (time_posted: string) => {
-    const date = new Date();
-    const changeDate = dayjs(time_posted).format("YYYY-MM-DD HH:mm:ss");
-    console.log(changeDate);
+    const now = dayjs();
+    const postDate = dayjs(time_posted).tz('UTC');
+    console.log("now", now);
+
+    const hoursDifference = now.diff(postDate, "hour");
+    const daysDifference = now.diff(postDate, "day");
+    const monthsDifference = now.diff(postDate, "month");
+    console.log("hoursDifference", hoursDifference);
+
+    let displayTime = "";
+
+    if (hoursDifference < 24) {
+      displayTime = `${hoursDifference} hour${
+        hoursDifference === 1 ? "" : "s"
+      } ago`;
+    } else if (daysDifference < 30) {
+      displayTime = `${daysDifference} day${
+        daysDifference === 1 ? "" : "s"
+      } ago`;
+    } else {
+      displayTime = `${monthsDifference} month${
+        monthsDifference === 1 ? "" : "s"
+      } ago`;
+    }
+
     // const dateAgoCalucated = dayjs(changeDate).fromNow()
-    console.log(date);
-    // setDateAgo(dateAgoCalucated)
+    // console.log(dateAgoCalucated);
+    setDateAgo(displayTime);
   };
 
   useEffect(() => {
@@ -49,7 +73,9 @@ const JobCard = ({
       <p className="text-gray-500">{description}</p>
       <div className="flex justify-between">
         <h1 className="text-blue-600 text-sm font-bold">{location}</h1>
-        <Button type="primary" size="large">Apply Now</Button>
+        <Button type="primary" size="large">
+          Apply Now
+        </Button>
       </div>
     </div>
   );
