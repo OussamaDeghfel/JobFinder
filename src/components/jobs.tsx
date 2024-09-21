eb143228daa233b768ddf19e2364d84a4e381f09
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-21 15:32:42 +0100

    fix the styling responsivity

--- src/components/jobs.tsx
+++ src/components/jobs.tsx
@@ -11,15 +11,18 @@ export interface dataJobType {
   time_posted: string;
 }
 interface JobsProps {
-  titleSearch?: string
-  locationSearch?: string
-  workTypeSearch?: string
+  titleSearch?: string;
+  locationSearch?: string;
+  workTypeSearch?: string;
   jobs?: dataJobType[];
 }
 
-
-const Jobs:React.FC<JobsProps> = ({ jobs, titleSearch, locationSearch, workTypeSearch }) => {
-
+const Jobs: React.FC<JobsProps> = ({
+  jobs,
+  titleSearch,
+  locationSearch,
+  workTypeSearch,
+}) => {
   useMemo(() => {
     if (titleSearch) {
       const filteredJobs = jobs?.filter((job) => {
@@ -30,41 +33,49 @@ const Jobs:React.FC<JobsProps> = ({ jobs, titleSearch, locationSearch, workTypeS
     }
     if (locationSearch) {
       const filteredJobs = jobs?.filter((job) => {
-        return job.location.toLowerCase().includes(locationSearch.toLowerCase());
+        return job.location
+          .toLowerCase()
+          .includes(locationSearch.toLowerCase());
       });
       jobs = filteredJobs;
     }
     if (workTypeSearch) {
       const filteredJobs = jobs?.filter((job) => {
-        return job.work_method.toLowerCase().includes(workTypeSearch.toLowerCase());
+        return job.work_method
+          .toLowerCase()
+          .includes(workTypeSearch.toLowerCase());
       });
       jobs = filteredJobs;
     }
-  },[titleSearch, locationSearch, workTypeSearch])
+  }, [titleSearch, locationSearch, workTypeSearch]);
 
   return (
-    <div className="grid lg:content-center sm:grid-cols-2 lg:grid-cols-3 justify-center translate-y-20">
+    <div className="flex flex-col translate-y-20">
       {jobs?.length == 0 ? (
-        <div className="flex flex-col justify-center items-center w-full h-full space-y-5 border-2 border-blue-500">
-          <h1 className="text-3xl font-bold">No Jobs Matches Your Searching</h1>
+        <div className="flex flex-col justify-center items-center w-full h-full space-y-5">
+          <h1 className="text-3xl font-bold">"No Jobs Match Your Search."</h1>
           <p className="text-base text-center">
-            Sorry, we couldn't find any jobs that match your search. Try adjusting your filters or keywords and check back later, as new opportunities are posted regularly!
+            Sorry, we couldn't find any jobs that match your search. Try
+            adjusting your filters or keywords and check back later, as new
+            opportunities are posted regularly!
           </p>
         </div>
       ) : (
-        jobs?.map((job) => (
-          <div key={job.id}>
-            <JobCard
-              title={job.title}
-              location={job.location}
-              work_method={job.work_method}
-              description={job.description}
-              time_posted={job.time_posted}
-            />
-          </div>
-        ))
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 x">
+          {jobs?.map((job) => (
+            <div key={job.id}>
+              <JobCard
+                title={job.title}
+                location={job.location}
+                work_method={job.work_method}
+                description={job.description}
+                time_posted={job.time_posted}
+              />
+            </div>
+          ))}
+        </div>
       )}
-      
+
       {/* {jobs?.map((job) => (
         <div key={job.id}>
           <JobCard
