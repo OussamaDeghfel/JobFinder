3f82eb80b072532378068df5f0f13ebc27b9e79f
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-22 16:32:42 +0100

    enh: restyling

--- src/components/jobFinderHome.tsx
+++ src/components/jobFinderHome.tsx
@@ -7,7 +7,7 @@ const JobFinderHome = () => {
   return (
     <div className="flex flex-col h-28 w-full bg-blue-500 p-5 rounded-b-full">
       <NavBar />
-      <div className="flex flex-col items-center m-auto">
+      <div className="flex flex-col items-center m-auto -translate-y-8">
         <Routes>
           <Route path="/" element={<Filters />} />
           <Route path="/applyingForm" element={<ApplyingForm />} />
