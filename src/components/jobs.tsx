68c9712c5c5977c4c07290e6124f5f224644aa94
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-23 17:39:09 +0100

    darkmode edit

--- src/components/jobs.tsx
+++ src/components/jobs.tsx
@@ -3,7 +3,7 @@ import { useMemo } from "react";
 import JobCard from "./jobCard";
 
 export interface dataJobType {
-  id: number;
+  id:string;
   title: string;
   location: string;
   work_method: string;
@@ -53,8 +53,8 @@ const Jobs: React.FC<JobsProps> = ({
     <div className="flex flex-col translate-y-20">
       {jobs?.length == 0 ? (
         <div className="flex flex-col justify-center items-center w-full h-full space-y-5">
-          <h1 className="text-3xl font-bold">"No Jobs Match Your Search."</h1>
-          <p className="text-base text-center">
+          <h1 className="text-3xl font-bold dark:text-white">"No Jobs Match Your Search."</h1>
+          <p className="text-base text-center dark:text-gray-300">
             Sorry, we couldn't find any jobs that match your search. Try
             adjusting your filters or keywords and check back later, as new
             opportunities are posted regularly!
