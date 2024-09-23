3bbe90e2436ca1779945dcaab22a096f9a42c0f4
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-23 16:32:43 +0100

    darkmode color fixing

--- src/components/applyingForm.tsx
+++ src/components/applyingForm.tsx
@@ -8,9 +8,9 @@ const ApplyingForm = () => {
     <>
       <NavBar />
       <div className="flex justify-center items-center -translate-y-4 ">
-        <div className=" bg-gray-50 z-10 w-[50%] h-screen p-8 rounded-md drop-shadow-lg space-y-8 dark:bg-slate-400">
-          <div className="flex flex-col justify-center items-center mb-8 space-y-2">
-            <h1 className="text-3xl font-bold">Job Application Form</h1>
+        <div className=" bg-gray-50 z-10 w-[50%] h-screen p-8 rounded-md drop-shadow-lg space-y-8 dark:bg-slate-500">
+          <div className="flex flex-col justify-center items-center mb-8 space-y-2 dark:text-white">
+            <h1 className="text-3xl font-bold ">Job Application Form</h1>
             <p>
               Please Fill Out the Form Below to Submit Your Job Application!
             </p>
@@ -19,20 +19,24 @@ const ApplyingForm = () => {
           <Form layout="vertical">
             <div className="flex justify-around">
               <Form.Item
+                label={<span className="dark:text-white">First Name</span>}
                 name="firstname"
-                label="First Name"
                 rules={[
                   {
                     required: true,
                     message: "Please input your FirstName!",
                   },
                 ]}
+                style={{ color: "white" }}
               >
-                <Input placeholder="First Name" className=" dark:bg-slate-300 dark:text-black" />
+                <Input
+                  placeholder="First Name"
+                  className=" dark:bg-slate-300 dark:text-black"
+                />
               </Form.Item>
               <Form.Item
+                label={<span className="dark:text-white">Last Name</span>}
                 name="lastname"
-                label="Last Name"
                 rules={[
                   {
                     required: true,
@@ -40,13 +44,16 @@ const ApplyingForm = () => {
                   },
                 ]}
               >
-                <Input placeholder="Last Name" />
+                <Input
+                  placeholder="Last Name"
+                  className=" dark:bg-slate-300 dark:text-black"
+                />
               </Form.Item>
             </div>
             <div className="flex px-12">
               <Form.Item
                 name="email"
-                label="Email"
+                label={<span className="dark:text-white">Email</span>}
                 rules={[
                   {
                     required: true,
@@ -55,13 +62,16 @@ const ApplyingForm = () => {
                 ]}
                 className="w-full px-10"
               >
-                <Input placeholder="Enter your email" />
+                <Input
+                  placeholder="Enter your email"
+                  className=" dark:bg-slate-300 dark:text-black"
+                />
               </Form.Item>
             </div>
             <div className="flex px-12">
               <Form.Item
                 name="coverletter"
-                label="Cover Letter"
+                label={<span className="dark:text-white">Cover Letter</span>}
                 rules={[
                   {
                     required: true,
@@ -72,39 +82,47 @@ const ApplyingForm = () => {
               >
                 <TextArea
                   size="large"
-                  className=""
+                  className=" dark:bg-slate-300 dark:text-black"
                   placeholder="Write a brief description about yourself (e.g., your skills, experience, and goals)"
                 />
               </Form.Item>
             </div>
             <div className="flex px-12">
-            <Form.Item label="Upload Resume" className="w-full px-10">
               <Form.Item
-                name="Upload Resume"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please Upload your Resume.",
-                  },
-                ]}
-                className="bg-white rounded-md"
+                label={<span className="dark:text-white">Upload Resume</span>}
+                className="w-full px-10"
               >
-                <Upload.Dragger name="Resume">
-                  <div className="flex items-center justify-center">
-                    <p>
-                      <BiUpload size={40} />
+                <Form.Item
+                  name="Upload Resume"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please Upload your Resume.",
+                    },
+                  ]}
+                  className="bg-white rounded-md dark:bg-slate-300 dark:text-black"
+                >
+                  <Upload.Dragger name="Resume">
+                    <div className="flex items-center justify-center">
+                      <p>
+                        <BiUpload size={40} />
+                      </p>
+                    </div>
+                    <p className="text-xl">Upload A File</p>
+                    <p className="text-sm font-thin">
+                      Drag and drop a file in this area.
                     </p>
-                  </div>
-                  <p className="text-xl">Upload A File</p>
-                  <p className="text-sm font-thin">
-                    Drag and drop a file in this area.
-                  </p>
-                </Upload.Dragger>
+                  </Upload.Dragger>
+                </Form.Item>
               </Form.Item>
-            </Form.Item>
             </div>
             <Form.Item className="flex w-full px-10 justify-center items-center ">
-              <Button type="primary" htmlType="submit" size="large">
+              <Button
+                type="primary"
+                htmlType="submit"
+                size="large"
+                className="dark:bg-blue-500"
+              >
                 Apply
               </Button>
             </Form.Item>
