d61af995391272fb455fa971b828c706e7122a6e
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-09-23 12:05:55 +0100

    add darkmode theme

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,11 +1,25 @@
 import { Switch } from 'antd'
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { BiMoon, BiSun } from 'react-icons/bi'
 import { Link } from 'react-router-dom'
 
 const NavBar = () => {
+const [darkMode, setDarkMode] = useState(() => {
+  return localStorage.getItem('theme') === 'light'
+})
+
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark')
+      localStorage.setItem('theme', 'dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+      localStorage.setItem('theme', 'light')
+    }
+  }, [darkMode])
   return (
-    <div className='flex flex-col h-28 w-full bg-blue-500 p-5 rounded-b-full'>
+    <div className='flex flex-col h-28 w-full dark:bg-slate-800 bg-blue-500 p-5 rounded-b-full'>
     <div className="flex w-[980px] h-12 justify-between items-center m-auto ">
         <h1 className="text-2xl font-bold text-white cursor-pointer">
             <Link to="/">
@@ -16,7 +30,7 @@ const NavBar = () => {
             {" "}
             <BiSun size={20} color="white" />{" "}
           </span>
-          <Switch defaultChecked size="default" />
+          <Switch size="default" onClick={() => setDarkMode(!darkMode)} />
           <span className="">
             {" "}
             <BiMoon size={20} color="white" />{" "}
