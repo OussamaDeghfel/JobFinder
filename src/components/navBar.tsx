51833bd74a635693238bab39ebd8a32360bfda1e
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-12-04 15:05:51 +0100

    fix the mode chagne when re-rendring to the first page and refresh it

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,24 +1,40 @@
-import { Switch } from "antd";
-import { useState } from "react";
-// import { useEffect, useState } from 'react'
+// import { Switch } from "antd";
+import { useEffect, useState } from 'react'
 import { BiMoon, BiSun } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+
+  const [sun, setSun] = useState<boolean | null>(null);
+  const [moon, setMoon] = useState<boolean | null>(null);
   const changeTheme = () => {
     const darkMode = localStorage.getItem("theme") === "light";
     if (darkMode) {
       document.documentElement.classList.add("dark");
       localStorage.setItem("theme", "dark");
+      setMoon(true);
+      setSun(false);
     } else {
       document.documentElement.classList.remove("dark");
       localStorage.setItem("theme", "light");
+      setMoon(false);
+      setSun(true);
     }
   };
 
-  // useEffect(() => {
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+      setMoon(true);
+      setSun(false);
+    } else {
+      document.documentElement.classList.remove("dark");
+      setMoon(false);
+      setSun(true);
+    }
 
-  // }, [])
+  }, [])
   return (
     <div className="flex flex-col h-28 w-full dark:bg-slate-800 bg-blue-500 p-5 rounded-b-full">
       <div className="flex w-[980px] h-12 justify-between items-center m-auto ">
@@ -26,15 +42,19 @@ const NavBar = () => {
           <Link to="/">JobFinder</Link>
         </h1>
         <div className="flex space-x-2">
-          <span className="">
+          {sun && (<span className="cursor-pointer" onClick={() => changeTheme()}>
             {" "}
             <BiSun size={20} color="white" />{" "}
-          </span>
-          <Switch size="default" onClick={() => changeTheme()} />
-          <span className="">
+          </span>)}
+          
+          {/* <Switch size="default"  /> */}
+          {moon && (
+            <span className="cursor-pointer" onClick={() => changeTheme()}>
             {" "}
             <BiMoon size={20} color="white" />{" "}
           </span>
+          )}
+          
         </div>
       </div>
     </div>
