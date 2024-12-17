16aa4e8ee0e207dd5594181a71ca695872a66b96
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-12-17 21:56:13 +0100

    implement the use of routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import NavBar from "./components/navBar";
 import Filters from "./components/filters";
-import ApplyingForm from "./components/applyingForm";
 // import JobFinderHome from "./components/jobFinderHome";
+import NavBar from "./components/navBar";
+import ApplyingForm from "./components/applyingForm";
 
 function App() {
   return (
     <>
-      <div className="w-full h-screen border-2 border-red-500">
-        <div className="flex flex-col h-28 w-full bg-blue-500 dark:bg-slate-800 p-5 rounded-b-full">
+      <div>
+        <BrowserRouter>
           <NavBar />
-          <div className="flex flex-col items-center m-auto -translate-y-8">
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Filters />} />
-                <Route path="/applyingForm" element={<ApplyingForm />} />
-              </Routes>
-            </BrowserRouter>
-          </div>
-        </div>
+          <Routes>
+            <Route path="/" element={<Filters />} />
+            <Route path="/applyingForm" element={<ApplyingForm />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   );
