948d125fb316997bf57252a3390b5c4e3b4a5ed8
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-12-18 20:13:39 +0100

    implement the success of submition and show the succces of applying component

--- src/components/applyingForm.tsx
+++ src/components/applyingForm.tsx
@@ -1,6 +1,7 @@
-import { Button, Form, Input, Select, Upload } from "antd";
-import { BiUpload } from "react-icons/bi";
+import { Button, Form, Input, Select } from "antd";
+// import { BiUpload } from "react-icons/bi";
 import { useEffect, useState } from "react";
+import SuccessApply from "./successApply";
 
 const ApplyingForm = () => {
   const { TextArea } = Input;
@@ -10,7 +11,7 @@ const ApplyingForm = () => {
     work_method: "",
     location: "",
   });
-  const [isFormValid, setIsFormValid] = useState([]);
+  const [isFormValid, setIsFormValid] = useState(false);
 
   const [form] = Form.useForm();
 
@@ -25,150 +26,164 @@ const ApplyingForm = () => {
 
   const handleValueChanges = () => {
     const firstname = form.getFieldValue("firstname");
-    setIsFormValid(firstname);
+    const lastname = form.getFieldValue("lastname");
+    const email = form.getFieldValue("email");
+    const coverletter = form.getFieldValue("coverletter");
+    const salaryrange = form.getFieldValue("salaryrange");
+
+    if (firstname && lastname && email && coverletter && salaryrange) {
+      setIsFormValid(true);
+    }
   };
 
   const handleSubmit = () => {
-    console.log(isFormValid)
-  }
+    console.log(isFormValid);
+  };
 
   return (
     <>
-      <div className="flex justify-evenly items-center -translate-y-4 ">
-        <div className="bg-gray-50 z-10 w-[50vh] h-[50%] p-8 rounded-md shadow-lg dark:bg-slate-500 border-2 border-gray-200 space-y-8">
-          <h1 className="font-bold text-xl">
-            You Are Applying for this Role :{" "}
-          </h1>
-          <div className="flex flex-col items-start text-gray-500 space-y-5">
-            <h1 className="text-3xl font-bold dark:text-white">
-              {roleData.title}
+      {!isFormValid && (
+        <div className="flex justify-evenly items-center -translate-y-4 ">
+          <div className="bg-gray-50 z-10 w-[50vh] h-[50%] p-8 rounded-md shadow-lg dark:bg-slate-500 border-2 border-gray-200 space-y-8">
+            <h1 className="font-bold text-xl">
+              You Are Applying for this Role :{" "}
             </h1>
-            <p className="text-gray-500 dark:text-gray-300">
-              {roleData.description}
-            </p>
-            <span className="dark:text-gray-300">{roleData.work_method}</span>
-            <h1 className="text-blue-600 text-sm font-bold dark:text-blue-800">
-              {roleData.location}
-            </h1>
+            <div className="flex flex-col items-start text-gray-500 space-y-5">
+              <h1 className="text-3xl font-bold dark:text-white">
+                {roleData.title}
+              </h1>
+              <p className="text-gray-500 dark:text-gray-300">
+                {roleData.description}
+              </p>
+              <span className="dark:text-gray-300">{roleData.work_method}</span>
+              <h1 className="text-blue-600 text-sm font-bold dark:text-blue-800">
+                {roleData.location}
+              </h1>
+            </div>
           </div>
-        </div>
-        <div className=" bg-gray-50 z-10 w-[50%] h-fit p-8 rounded-md drop-shadow-lg space-y-8 dark:bg-slate-500">
-          <div className="flex flex-col justify-center items-center mb-8 space-y-2 dark:text-white">
-            <h1 className="text-3xl font-bold ">Job Application Form</h1>
-            <p>
-              Please Fill Out the Form Below to Submit Your Job Application!
-            </p>
-          </div>
-          <div className="w-full h-0.5 bg-gray-300 dark:bg-slate-700"></div>
-          <Form
-            form={form}
-            layout="vertical"
-            style={{ height: "100%" }}
-            onValuesChange={handleValueChanges}
-            onFinish={handleSubmit}
-          >
-            <div className="flex flex-col justify-between h-full overflow-hidden">
-              <div className="flex justify-between px-20">
-                <Form.Item
-                  label={<span className="dark:text-white">First Name</span>}
-                  name="firstname"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please input your FirstName!",
-                    },
-                  ]}
-                  style={{ color: "white" }}
-                >
-                  <Input
-                    placeholder="First Name"
-                    className=" dark:bg-slate-300 dark:text-black"
-                  />
-                </Form.Item>
-                <Form.Item
-                  label={<span className="dark:text-white">Last Name</span>}
-                  name="lastname"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please input your LasName!",
-                    },
-                  ]}
-                >
-                  <Input
-                    placeholder="Last Name"
-                    className=" dark:bg-slate-300 dark:text-black"
-                  />
-                </Form.Item>
-              </div>
-              <div className="flex px-10">
-                <Form.Item
-                  name="email"
-                  label={<span className="dark:text-white">Email</span>}
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please input your Email Address!",
-                    },
-                  ]}
-                  className="w-full px-10"
-                >
-                  <Input
-                    placeholder="Enter your email"
-                    className=" dark:bg-slate-300 dark:text-black"
-                  />
-                </Form.Item>
-              </div>
-              <div className="flex px-10">
-                <Form.Item
-                  name="coverletter"
-                  label={<span className="dark:text-white">Cover Letter</span>}
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please do not exceed 200 words.",
-                    },
-                  ]}
-                  className="w-full h-fit px-10"
-                >
-                  <TextArea
-                    size="large"
-                    className=" dark:bg-slate-300 dark:text-black"
-                    placeholder="Write a brief description about yourself (e.g., your skills, experience, and goals)"
-                  />
-                </Form.Item>
-              </div>
-              <div className="flex px-10">
-                <Form.Item
-                  name="salaryrange"
-                  label={<span className="dark:text-white">Salary Range</span>}
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please select your salary range.",
-                    },
-                  ]}
-                  className="w-full px-10"
-                >
-                  <Select placeholder="Select your salary range" options={
-                    [
+          <div className=" bg-gray-50 z-10 w-[50%] h-fit p-8 rounded-md drop-shadow-lg space-y-8 dark:bg-slate-500">
+            <div className="flex flex-col justify-center items-center mb-8 space-y-2 dark:text-white">
+              <h1 className="text-3xl font-bold ">Job Application Form</h1>
+              <p>
+                Please Fill Out the Form Below to Submit Your Job Application!
+              </p>
+            </div>
+            <div className="w-full h-0.5 bg-gray-300 dark:bg-slate-700"></div>
+            <Form
+              form={form}
+              layout="vertical"
+              style={{ height: "100%" }}
+              onValuesChange={handleValueChanges}
+              onFinish={handleSubmit}
+            >
+              <div className="flex flex-col justify-between h-full overflow-hidden">
+                <div className="flex justify-between px-20">
+                  <Form.Item
+                    label={<span className="dark:text-white">First Name</span>}
+                    name="firstname"
+                    rules={[
                       {
-                        value: "1",
-                        label: "200$ - 500$"
+                        required: true,
+                        message: "Please input your FirstName!",
                       },
+                    ]}
+                    style={{ color: "white" }}
+                  >
+                    <Input
+                      placeholder="First Name"
+                      className=" dark:bg-slate-300 dark:text-black"
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    label={<span className="dark:text-white">Last Name</span>}
+                    name="lastname"
+                    rules={[
                       {
-                        value: "2",
-                        label: "500$ - 1000$"
+                        required: true,
+                        message: "Please input your LasName!",
                       },
+                    ]}
+                  >
+                    <Input
+                      placeholder="Last Name"
+                      className=" dark:bg-slate-300 dark:text-black"
+                    />
+                  </Form.Item>
+                </div>
+                <div className="flex px-10">
+                  <Form.Item
+                    name="email"
+                    label={<span className="dark:text-white">Email</span>}
+                    rules={[
                       {
-                        value: "3",
-                        label: "1000$ - 2000$"
+                        required: true,
+                        message: "Please input your Email Address!",
                       },
-                    ]
-                  } /> 
-                </Form.Item>
-              </div>
-              {/* <div className="flex px-10">
+                    ]}
+                    className="w-full px-10"
+                  >
+                    <Input
+                      placeholder="Enter your email"
+                      className=" dark:bg-slate-300 dark:text-black"
+                    />
+                  </Form.Item>
+                </div>
+                <div className="flex px-10">
+                  <Form.Item
+                    name="coverletter"
+                    label={
+                      <span className="dark:text-white">Cover Letter</span>
+                    }
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please do not exceed 200 words.",
+                      },
+                    ]}
+                    className="w-full h-fit px-10"
+                  >
+                    <TextArea
+                      size="large"
+                      className=" dark:bg-slate-300 dark:text-black"
+                      placeholder="Write a brief description about yourself (e.g., your skills, experience, and goals)"
+                    />
+                  </Form.Item>
+                </div>
+                <div className="flex px-10">
+                  <Form.Item
+                    name="salaryrange"
+                    label={
+                      <span className="dark:text-white">Salary Range</span>
+                    }
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please select your salary range.",
+                      },
+                    ]}
+                    className="w-full px-10"
+                  >
+                    <Select
+                      placeholder="Select your salary range"
+                      options={[
+                        {
+                          value: "1",
+                          label: "200$ - 500$",
+                        },
+                        {
+                          value: "2",
+                          label: "500$ - 1000$",
+                        },
+                        {
+                          value: "3",
+                          label: "1000$ - 2000$",
+                        },
+                      ]}
+                      allowClear
+                    />
+                  </Form.Item>
+                </div>
+                {/* <div className="flex px-10">
                 <Form.Item
                   label={<span className="dark:text-white">Upload Resume</span>}
                   className="w-full px-10"
@@ -197,20 +212,27 @@ const ApplyingForm = () => {
                   </Form.Item>
                 </Form.Item>
               </div> */}
-              <Form.Item className="flex w-full px-10 justify-center items-center ">
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  size="large"
-                  className="dark:bg-blue-500"
-                >
-                  Apply
-                </Button>
-              </Form.Item>
-            </div>
-          </Form>
+                <Form.Item className="flex w-full px-10 justify-center items-center ">
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    size="large"
+                    className="dark:bg-blue-500"
+                  >
+                    Apply
+                  </Button>
+                </Form.Item>
+              </div>
+            </Form>
+          </div>
         </div>
-      </div>
+      )}
+
+      {isFormValid && (
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+        <SuccessApply />
+        </div>
+      )}
     </>
   );
 };
