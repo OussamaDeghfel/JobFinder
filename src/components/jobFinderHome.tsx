246fd0a106ff7ff80703cfb56ff64aa9dd3cb6eb
OussamaDeghfel <oussama.deghfel@gmail.com>
2025-01-04 19:46:51 +0100

    remove routing from jobfinder component

--- src/components/jobFinderHome.tsx
+++ src/components/jobFinderHome.tsx
@@ -1,19 +1,15 @@
 import Filters from "./filters";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ApplyingForm from "./applyingForm";
-import NavBar from "./navBar";
 
 const JobFinderHome = () => {
   return (
     <div className="flex flex-col h-28 w-full bg-blue-500 dark:bg-slate-800 p-5 rounded-b-full">
-      <NavBar />
+      {/* <NavBar /> */}
       <div className="flex flex-col items-center m-auto -translate-y-8">
-        <BrowserRouter>
-        <Routes>
+        {/* <Routes>
           <Route path="/" element={<Filters />} />
           <Route path="/applyingForm" element={<ApplyingForm />} />
-        </Routes>
-        </BrowserRouter>
+        </Routes> */}
+        <Filters />
       </div>
     </div>
   );
